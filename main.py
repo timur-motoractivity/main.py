576565175473721671fb8e5af55dc6787404ac2c
timur-motoractivity <timakagarmanov@gmail.com>
2024-08-21 19:05:53 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+#"1st programm"
+a = (9 ** 0.5)*5
+print(a)
+
+#"2nd programm"
+b = 9.99>9.98 and 1000!=1000.1
+print(b)
+
+#"3rd programm"
+c = 2*2+2
+d = 2*(2+2)
+e = c==d
+print(e)
+
+#"4th programm"
+import math
+
+string = '123.456'
+floating = float(string)
+digit = math.floor((floating * 10) %10)
+
+print(digit)
+
